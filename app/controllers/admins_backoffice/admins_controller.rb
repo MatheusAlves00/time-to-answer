cd9258c78285af09e3f8c8bfc3937befb5a925ac
MatheusAlves00 <m_alves11@yahoo.com.br>
2021-03-24 16:01:59 -0300

    Corrigindo um erro do formulário

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -8,8 +8,13 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def update
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+
     @admin = Admin.find(params[:id])
     param_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
+
     if @admin.update(param_admin)
       redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso!"
     else
